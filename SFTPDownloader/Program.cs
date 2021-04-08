a3d28554c3d037ac7d9db4e99cc28f875fe7f903
DAthukorala <59525154+DAthukorala@users.noreply.github.com>
2021-04-08 22:42:13 +1200

    added multi folder download support

--- SFTPDownloader/Program.cs
+++ SFTPDownloader/Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using System.Linq;
+using Flurl;
+using System.Diagnostics;
 
 namespace SFTPDownloader
 {
@@ -13,6 +16,10 @@ namespace SFTPDownloader
     {
         static async Task Main(string[] args)
         {
+            //move files after download to another folder (after all secondary server backups are completed)
+            //sync the same folder structure
+
+
             var config = SetupConfiguration();
             Rebex.Licensing.Key = config["Key"];
 
@@ -30,28 +37,62 @@ namespace SFTPDownloader
         private async static Task<Sftp> Connect(IConfigurationRoot config)
         {
             var client = new Sftp();
+            Console.WriteLine($"Connecting to server {config["Server"]}");
             await client.ConnectAsync(config["Server"]);
+            Console.WriteLine("Logging in...");
             await client.LoginAsync(config["UserName"], config["Password"]);
+            Console.WriteLine("Logged In");
             return client;
         }
         private async static Task Download(Sftp client, IConfigurationRoot config)
         {
             var remotePath = config["RemotePath"];
             var localPath = config["LocalPath"];
-            var fileInfo = await client.GetItemsAsync($"{remotePath}/*", TraversalMode.MatchFilesShallow);
+            var fileType = config["FileType"];
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Console.WriteLine("Fetching directory names...");
+            var directoryNames = client.GetList($"{remotePath}/").Cast<SftpItem>().Where(item => item.IsDirectory).Select(item => item.Name);
+
             var bulkhead = Policy.BulkheadAsync(100, int.MaxValue);
             var tasks = new List<Task>();
-            foreach (var info in fileInfo)
+            long downloadSize = 0;
+            foreach (var name in directoryNames)
             {
-                var task = bulkhead.ExecuteAsync(async () =>
-                {
-                    var bytesDownloaded = await client.GetFileAsync($"{remotePath}/{info.Name}", @$"{localPath}\{info.Name}");
-                    Console.WriteLine($"bytesDownloaded: {bytesDownloaded}");
+                var remoteDirPath = Url.Combine(remotePath, name).ToString();
+                var localDirPath = Path.Combine(localPath, name).ToString();
 
-                });
-                tasks.Add(task);
+                Console.WriteLine($"Fetching file names for {remoteDirPath}");
+                var pattern = new FileSet(remoteDirPath, $"*.{fileType}", TraversalMode.MatchFilesShallow);
+                var fileInfo = await client.GetItemsAsync(pattern);
+                foreach (var info in fileInfo)
+                {
+                    if (!info.IsDirectory)
+                    {
+                        var fullFileName = info.Name;
+                        var remoteFilePath = Url.Decode(Url.Combine(remoteDirPath, fullFileName), false).ToString();
+                        var localFilePath = Path.Combine(localDirPath, fullFileName).ToString();
+
+                        var task = bulkhead.ExecuteAsync(async () =>
+                        {
+                            var bytesDownloaded = await client.GetFileAsync(remoteFilePath, localFilePath);
+                            downloadSize += bytesDownloaded;
+                            Console.WriteLine($"Download from {remoteFilePath} to ${localFilePath}, size: {bytesDownloaded}");
+
+                        });
+                        tasks.Add(task);
+                    }
+                }
             }
+
             await Task.WhenAll(tasks);
+            stopWatch.Stop();
+            var elapsedTime = stopWatch.Elapsed;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Download Time {elapsedTime.Hours}h:{elapsedTime.Minutes}m:{elapsedTime.Seconds}s:{elapsedTime.Milliseconds}ms");
+            Console.WriteLine($"Download Size: {downloadSize}");
+            Console.ResetColor();
+            Console.Read();
         }
     }
 }
